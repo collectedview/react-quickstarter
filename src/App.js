049fea726dd1b7c43e9f5e4c436ccb603b5eab42
Joe Alongi <collectedview@gmail.com>
2021-01-16 22:22:46 -0500

    Version 2
    Rebuilt application & updated dependencies.
    
    Remove Firebase Folder
    
    React Quickstarter Banner
    
    Drop Axios
    
    Create FUNDING.yml

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { Fragment } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import "./App.css";
 
-import Header from './components/Header';
-import RightSide from './components/RightSide';
-import LeftSide from './components/LeftSide';
+import Navbar from "./components/Navbar";
+import Layout from "./components/Higher-Order-Components/Layout";
+import Footer from "./components/Footer";
 
-const App = () => (
-  <Fragment>
-  
-    <Header />
-    
-    <main className="my-5 py-5">
-      <Container className="px-0">
-        <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
-        
-          <Col xs={{ order: 2 }} md={{ size: 4, order: 1 }} tag="aside" className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
-            <LeftSide />
-          </Col>
-          
-          <Col xs={{ order: 1 }} md={{ size: 7, offset: 1 }} tag="section" className="py-5 mb-5 py-md-0 mb-md-0">
-            <RightSide />
-          </Col>
-          
-        </Row>
-      </Container>
-    </main>
-    
-  </Fragment>
-);
+function App() {
+  return (
+    <>
+      <div className="m-1 p-1">
+        <Navbar />
+        <Layout />
+        <Footer />
+      </div>
+    </>
+  );
+}
 
 export default App;
